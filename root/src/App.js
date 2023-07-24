df21231c3c09ef1b35c215a17dfe876b45df3f38
jacknmorris <jacknmorris@yahoo.com>
2023-07-24 19:17:15 +0100

    Initial commit

---
+++ root/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="header-container">
+      <Header />
+      {/* Add other components and content of your website here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ root/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>My header works with css</h1>
+      {/* Add other header elements here */}
+    </div>
+  );
+};
+
+export default Header;
